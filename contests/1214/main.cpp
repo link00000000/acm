207426a15ec9e233253a04605b3fc12184f2c897
Logan Crandall <crandall.logan@gmail.com>
2019-09-13 21:11:43 -0400

    contest 1214

---
+++ contests/1214/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+
+using namespace std;
+int main()
+{
+  int rubles, priceOfDollar, priceOfEuro;
+  cin >> rubles >> priceOfDollar >> priceOfEuro;
+
+  priceOfEuro *= 5;
+
+  int iteration = 0;
+  int min = -1;
+
+  while(true)
+  {
+    int remaining = (rubles - (iteration * priceOfEuro)) % priceOfDollar;
+    if(rubles < iteration * priceOfEuro)
+      break;
+    if(remaining < min || min == -1)
+      min = remaining;
+    ++iteration;
+  }
+
+  cout << min;
+  return 0;
+}
