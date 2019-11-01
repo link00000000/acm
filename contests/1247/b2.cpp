be3e5e372df7ef832cbc1b42f380b40677aa0d60
Logan Crandall <crandall.logan@gmail.com>
2019-11-01 18:11:30 -0400

    1247/b

---
+++ contests/1247/b2.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <set>
+#include <vector>
+
+using namespace std;
+
+int main() {
+
+  int numQueries;
+  cin >> numQueries;
+  while(numQueries--) {
+
+    int scheduleLength, numShows, length;
+    cin >> scheduleLength >> numShows >> length;
+
+    vector<int> schedule(scheduleLength);
+    for(int i = 0; i < scheduleLength; ++i) {
+      int show;
+      cin >> show;
+      schedule[i] = show;
+    }
+
+    int smallestSetLength = -1;
+    for(int i = 0; i < scheduleLength - length + 1; ++i) {
+      set<int> shows;
+      for(int j = 0; j < length; ++j) {
+        shows.insert(schedule[i + j]);
+      }
+      if(shows.size() < smallestSetLength || smallestSetLength == -1)
+        smallestSetLength = shows.size();
+      if(smallestSetLength == 1)
+        break;
+    }
+    cout << smallestSetLength << '\n';
+
+  }
+
+  return 0;
+}
